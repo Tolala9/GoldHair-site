e86ea5d6df899e7016adb01ed83455c6c0d26f17
Tolala9 <anatolii.lukavenko@gmail.com>
2017-09-09 12:16:37 +0300

    Make Section Callback adaptive for all devices

--- app/js/common.js
+++ app/js/common.js
@@ -52,6 +52,32 @@ $('.carousel-services').owlCarousel({
 						ths.find('.carousel-services-image').css('min-height', thsh);
 			});
 	}carouselService();
+
+	$('select').selectize({
+		create: true
+	});
+
+	//E-mail Ajax Send
+	$("form.callback").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "/mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$(th).find('.success').addClass('active').css('display', 'flex').hide().fadeIn();
+			setTimeout(function() {
+				// Done Functions
+				$(th).find('.success').removeClass('active').fadeOut();
+				th.trigger("reset");
+			}, 3000);
+		});
+		return false;
+	});
+
+	
+
+
 	
 });
 	
